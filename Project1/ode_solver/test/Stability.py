44dbcddc0792d833e42850133d8a94192bc25c90
GUDDETI SREETEJA <gsreeteja25@gmail.com>
2026-02-07 19:38:38 +0530

    Stability starting

--- Project1/ode_solver/test/Stability.py
+++ Project1/ode_solver/test/Stability.py
@@ -4,7 +4,7 @@ from methods.ABM_4 import ABM4
 from methods.RK_2 import RK2
 import matplotlib.pyplot as plt
 from methods.RK_4 import RK4
-from methods.ABM2 import ABM2
+from methods.ABM_2 import ABM2
 
 # Zaduinsky method
 def fu(u,x):
