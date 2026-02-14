118bc8fb901c46eee743088ebc35c3f979f4bfe1
devanthecoder <anju.blore2015@gmail.com>
2026-02-14 16:27:42 +0530

    Hopefully fixing accuracy.py once and for all

--- Project1/ode_solver/analysis/accuracy.py
+++ Project1/ode_solver/analysis/accuracy.py
@@ -120,7 +120,7 @@ ob = odesolver(order=2, method=RK2, bc=bc, tol=1e-8, max_iter=1000, func=f, gues
 methods = [(RK2, 'RK2'), (RK4, 'RK4'), (ABM2, 'ABM2'), (ABM4, 'ABM4')]
 
 # Step sizes to test
-h_values = [1e-3, 1e-4, 1e-5, 1e-6]
+h_values = [1e-3, 5e-3, 1e-4, 5e-4, 1e-5]
 
 # Keep originals to restore later
 original_h = ob.get_h()
