72c1fc5a516e6f5950023dee83a9c9463be5430c
devanthecoder <anju.blore2015@gmail.com>
2026-02-15 13:51:06 +0530

    Just testing limits of FDM

--- Project2/Test/DD.py
+++ Project2/Test/DD.py
@@ -1,21 +1,27 @@
 import json
 import numpy as np
+from numba import njit
 import matplotlib.pyplot as plt
-from Core.FDM_Solver import FDM_Solver
+# from Core.FDM_Solver import FDM_Solver
 from Core.Loader import load_config
+from Core.FDM_Solver import FDM_Solver
 
 # --- Main Execution ---
 
 config_path = 'config.json'
 try:
-    N, domain, BC, F, Fy, Fyp, analytical_solution, tol, max_iter = load_config(config_path)
+    N, domain, BC, F, Fy, Fyp, analytical_solution, tol, max_iter, error = load_config(config_path)
     print(f"Successfully loaded configuration from {config_path}")
 except FileNotFoundError:
     print(f"Error: Could not find {config_path}")
     exit()
+import numpy as np
+from typing import Callable
 
+
+#minor change
 # Initialize Solver
-solver = FDM_Solver(F, Fy, Fyp, N, domain, BC)
+solver = FDM_Solver(F, None, None, N, domain, BC)
 solution = solver.solver(tol=tol, max_iter=max_iter)
 
 print("Numerical Solution:", solution)
